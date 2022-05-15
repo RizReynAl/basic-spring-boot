12e44990cf95ee308ee600e67b04bfd697d3ebf5
rizreynal <reynaldirizki43@gmail.com>
2022-05-16 06:44:26 +0700

    Update Columbia test

--- src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
+++ src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
@@ -43,7 +43,7 @@ public class CountryOfResidenceService extends BaseService{
         }
 
         CountryOfResidence cor = new CountryOfResidence();
-        cor.setCor("Indonesia");
+        cor.setCor("NIGERIA");
         cor.setUid("1");
         cor.setLanguageCode("EN");
         cor.setPublishFlag(true);

a49296e2cab3a2a173bb32bf38bc329d53e59cfb
rizreynal <reynaldirizki43@gmail.com>
2022-05-16 14:02:44 +0700

    Update SG

--- src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
+++ src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
@@ -43,7 +43,7 @@ public class CountryOfResidenceService extends BaseService{
         }
 
         CountryOfResidence cor = new CountryOfResidence();
-        cor.setCor("AURO");
+        cor.setCor("SG");
         cor.setUid("1");
         cor.setLanguageCode("EN");
         cor.setPublishFlag(true);

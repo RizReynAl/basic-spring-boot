d62cb700127e8b0040305d8bb03e5d21c41b4831
rizreynal <reynaldirizki43@gmail.com>
2022-05-16 14:00:32 +0700

    Update AURO

--- src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
+++ src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
@@ -43,7 +43,7 @@ public class CountryOfResidenceService extends BaseService{
         }
 
         CountryOfResidence cor = new CountryOfResidence();
-        cor.setCor("BELGIA");
+        cor.setCor("AURO");
         cor.setUid("1");
         cor.setLanguageCode("EN");
         cor.setPublishFlag(true);

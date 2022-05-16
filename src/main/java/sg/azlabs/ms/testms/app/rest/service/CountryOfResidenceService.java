798cd270641c918504d70ef09e311685df6eae8e
rizreynal <reynaldirizki43@gmail.com>
2022-05-16 13:51:44 +0700

    Update BELGIA test

--- src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
+++ src/main/java/sg/azlabs/ms/testms/app/rest/service/CountryOfResidenceService.java
@@ -43,7 +43,7 @@ public class CountryOfResidenceService extends BaseService{
         }
 
         CountryOfResidence cor = new CountryOfResidence();
-        cor.setCor("Jepang");
+        cor.setCor("BELGIA");
         cor.setUid("1");
         cor.setLanguageCode("EN");
         cor.setPublishFlag(true);
